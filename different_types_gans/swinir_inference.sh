e9d2f6229b1b103797093b40d3db7428720bacdf
rishabh-mondal <rishabh.mondal19@gmail.com>
2025-01-28 00:46:23 +0530

    Run inference script in the background and update log message

--- different_types_gans/swinir_inference.sh
+++ different_types_gans/swinir_inference.sh
@@ -6,8 +6,8 @@ SCRIPT_PATH="/home/rishabh.mondal/Brick-Kilns-project/ijcai_2025_kilns/different
 # Log file path
 LOG_FILE="/home/rishabh.mondal/Brick-Kilns-project/ijcai_2025_kilns/different_types_gans/test_bihar_swinir_inference_4x.log"
 
-# Command to run the Python script and write output to the log file
-python $SCRIPT_PATH > $LOG_FILE 2>&1
+# Command to run the Python script in the background and write output to the log file
+nohup python $SCRIPT_PATH > $LOG_FILE 2>&1 &
 
-# Print a message to indicate the script has finished
-echo "Script execution completed. Logs are saved in $LOG_FILE"
+# Print a message to indicate the script is running in the background
+echo "Script is running in the background. Logs are saved in $LOG_FILE"
